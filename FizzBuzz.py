dc53eff6fed1cc73404af158d52c6e6dfe20bc9c
Montoussy Paul <paul.montoussy@gmail.com>
2025-09-18 13:38:18 +0200

    exercise done

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,23 +1,27 @@
 def fizzbuzz(a):
     word = ""
-    if a % 3 == 0 or a % 5 == 0:
-        if a % 3 == 0 and a % 5 == 0:
-             word += "FizzBuzz"
-        if a % 5 == 0 :
-             word += "Buzz"
-        elif a % 3 == 0:
-             word += "Fizz"
-        a = str(a)
-        for char in a:
-            if a == "3":
-                word += "Fizz"
-            if a == "5":
-                word += "Buzz"
+    b = str(a)
+
+    for char in b:
+        if char == "3":
+            word += "Fizz"
+        if char == "5":
+            word += "Buzz"
+
+    if a % 3 == 0:
+         word += "Fizz"
+    if a % 5 == 0 :
+         word += "Buzz"
+
+
+    if len(word) == 0:
+        return b
     return word
 
 def fizzbuzz_main():
     for a in range(1, 101):
-        print(fizzbuzz(a))
+
+        print("Index : ", a, " resultat => ",fizzbuzz(a))
 
 if __name__ == "__main__":
-    pass
+    fizzbuzz_main()
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,19 +2,19 @@ from FizzBuzz import fizzbuzz
 
 
 def test_fizz_buzz_three():
-    assert fizzbuzz(3) == "Fizz"
     assert fizzbuzz(6) == "Fizz"
     assert fizzbuzz(9) == "Fizz"
+    assert fizzbuzz(3) == "FizzFizz"
 
 def test_fizz_buzz_five():
-    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(5) == "BuzzBuzz"
 
 def test_fizz_buzz_other_number():
-    assert fizzbuzz(4) == 4
+    assert fizzbuzz(4) == "4"
 
 def test_fizz_buzz_both_three_five():
-    assert fizzbuzz(15) == "FizzBuzz"
-    assert fizzbuzz(30) == "FizzBuzz"
+    assert fizzbuzz(15) == "BuzzFizzBuzz"
+    assert fizzbuzz(30) == "FizzFizzBuzz"
 
 def test_fizz_buzz_13():
     assert fizzbuzz(13) == "Fizz"
